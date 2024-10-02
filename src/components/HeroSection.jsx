09556159c5fba6169313832069815cd645b0921d
Kenneth02658 <kenneth@senseitechnology.com>
2024-10-02 08:44:30 +0000

    Download CV button edited

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -50,11 +50,10 @@ function HeroSection() {
                         <p className='text-[#eeeeec] text-lg font-sans md:w-[60%] w-[100%] sm:line-clamp-none'>Driven by a passion for assisting clients and startups in realizing their dreams  through innovative web and mobile solutions.</p>
                     </div>
                     <div>
-                        <a className=''>
+                        <div>
                         <button class="
                             group
                             p-5
-                            cursor-pointer 
                             relative  
                             text-xl 
                             font-normal 
@@ -86,15 +85,14 @@ function HeroSection() {
                             <p class="group-hover:opacity-0 group-hover:translate-x-[-100%] absolute translate-x-0 transition-all
                             duration-200">Download CV</p>
 
-                            <span class="group-hover:translate-x-0  group-hover:opacity-100 absolute  translate-x-full opacity-0  transition-all duration-200">Thank
-                                you!
+                            <span class="group-hover:translate-x-0  group-hover:opacity-100 absolute  translate-x-full opacity-0  transition-all duration-200">Contact me
                             </span>
 
                             <span
                                 class="group-hover:w-full absolute right-0 h-full w-5  border-y border-r  border-[#f0c020] transition-all duration-500">
                             </span>
                         </button>
-                        </a>
+                        </div>
                     </div>
                     
                 </div>
